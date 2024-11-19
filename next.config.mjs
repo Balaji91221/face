eabd9e3a673b227fdb66e947d6e14eedc1aad535
K Balaji Naik <101827986+Balaji91221@users.noreply.github.com>
2024-11-19 15:29:53 +0530

    done

--- next.config.mjs
+++ next.config.mjs
@@ -3,23 +3,7 @@ const nextConfig = {
     images: {
         domains: ['lh3.googleusercontent.com'], // Add this line
       },
-      async headers() {
-        return [
-          {
-            source: "/(.*)", // Apply to all routes
-            headers: [
-              {
-                key: "Cross-Origin-Opener-Policy",
-                value: "same-origin", // You can also use "unsafe-none" depending on your needs
-              },
-              {
-                key: "Cross-Origin-Embedder-Policy",
-                value: "require-corp", // Enforces CORS for embedded resources
-              },
-            ],
-          },
-        ];
-      },
+      
 };
 
 export default nextConfig;
